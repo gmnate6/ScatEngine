aeff1002dc84ed38a0e457f03488e15d8818aa44
Nathan Holmberg <gmnate6@gmail.com>
2026-06-25 19:38:26 -0400

    Added tests for all of RoundEngine and Scoring

---
+++ Tests/ScatEngineTests/Engine/ScoringTests.swift
@@ -0,0 +1,108 @@
+import Testing
+@testable import ScatEngine
+
+struct ScoringTest {
+    @Test
+    func valueOfCardTest() {
+        #expect(Scoring.value(of: .init(rank: .queen, suit: .spades)) == 10)
+        #expect(Scoring.value(of: .init(rank: .two, suit: .hearts))   == 2)
+        #expect(Scoring.value(of: .init(rank: .ace, suit: .diamonds)) == 11)
+    }
+    
+    @Test
+    func scoreOfPlayerTest() {
+        var p1 = Player(name: "Test", chips: 3)
+        p1.addCard(Card(rank: .two, suit: .clubs))
+        p1.addCard(Card(rank: .ace, suit: .clubs))
+        p1.addCard(Card(rank: .jack, suit: .clubs))
+        #expect(Scoring.score(of: p1) == 23)
+        
+        var p2 = Player(name: "Test", chips: 3)
+        p2.addCard(Card(rank: .two, suit: .clubs))
+        p2.addCard(Card(rank: .ace, suit: .hearts))
+        p2.addCard(Card(rank: .jack, suit: .diamonds))
+        #expect(Scoring.score(of: p2) == 11)
+        
+        var p3 = Player(name: "Test", chips: 3)
+        p3.addCard(Card(rank: .two, suit: .clubs))
+        p3.addCard(Card(rank: .three, suit: .clubs))
+        p3.addCard(Card(rank: .jack, suit: .diamonds))
+        #expect(Scoring.score(of: p3) == 10)
+        
+        var p4 = Player(name: "Test", chips: 3)
+        p4.addCard(Card(rank: .two, suit: .clubs))
+        p4.addCard(Card(rank: .two, suit: .spades))
+        p4.addCard(Card(rank: .two, suit: .diamonds))
+        #expect(Scoring.score(of: p4) == 2)
+        
+        var p5 = Player(name: "Test", chips: 3)
+        p5.addCard(Card(rank: .ace, suit: .spades))
+        p5.addCard(Card(rank: .king, suit: .spades))
+        p5.addCard(Card(rank: .queen, suit: .spades))
+        #expect(Scoring.score(of: p5) == 31)
+    }
+    
+    @Test
+    func isScatTest() {
+        var p1 = Player(name: "Test", chips: 3)
+        p1.addCard(Card(rank: .ace, suit: .clubs))
+        p1.addCard(Card(rank: .king, suit: .clubs))
+        p1.addCard(Card(rank: .queen, suit: .clubs))
+        #expect(Scoring.isScat(player: p1))
+        
+        var p2 = Player(name: "Test", chips: 3)
+        p2.addCard(Card(rank: .ace, suit: .clubs))
+        p2.addCard(Card(rank: .king, suit: .diamonds))
+        p2.addCard(Card(rank: .queen, suit: .hearts))
+        #expect(!Scoring.isScat(player: p2))
+        
+        var p3 = Player(name: "Test", chips: 3)
+        p3.addCard(Card(rank: .two, suit: .diamonds))
+        p3.addCard(Card(rank: .three, suit: .diamonds))
+        p3.addCard(Card(rank: .four, suit: .diamonds))
+        #expect(!Scoring.isScat(player: p3))
+        
+        var p4 = Player(name: "Test", chips: 3)
+        p4.addCard(Card(rank: .four, suit: .clubs))
+        p4.addCard(Card(rank: .four, suit: .diamonds))
+        p4.addCard(Card(rank: .four, suit: .hearts))
+        #expect(!Scoring.isScat(player: p4))
+    }
+    
+    @Test
+    func hasScatTest() {
+        let rng = SeededGenerator(seed: 123)
+        
+        var p1 = Player(name: "Test", chips: 3)
+        p1.addCard(Card(rank: .four, suit: .clubs))
+        p1.addCard(Card(rank: .four, suit: .diamonds))
+        p1.addCard(Card(rank: .four, suit: .hearts))
+
+        var gameState = GameState(rng: rng, players: [p1])
+        #expect(!hasScat(gameState: gameState))
+        
+        var p2 = Player(name: "Test", chips: 3)
+        p2.addCard(Card(rank: .ace, suit: .clubs))
+        p2.addCard(Card(rank: .king, suit: .diamonds))
+        p2.addCard(Card(rank: .queen, suit: .hearts))
+        
+        gameState.players.append(p2)
+        #expect(!hasScat(gameState: gameState))
+        
+        var p3 = Player(name: "Test", chips: 3)
+        p3.addCard(Card(rank: .two, suit: .diamonds))
+        p3.addCard(Card(rank: .three, suit: .diamonds))
+        p3.addCard(Card(rank: .four, suit: .diamonds))
+        
+        gameState.players.append(p3)
+        #expect(!hasScat(gameState: gameState))
+        
+        var p4 = Player(name: "Test", chips: 3)
+        p4.addCard(Card(rank: .ace, suit: .clubs))
+        p4.addCard(Card(rank: .king, suit: .clubs))
+        p4.addCard(Card(rank: .queen, suit: .clubs))
+
+        gameState.players.append(p4)
+        #expect(hasScat(gameState: gameState))
+    }
+}
